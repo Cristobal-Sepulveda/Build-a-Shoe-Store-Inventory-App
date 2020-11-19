d78108c7a0bcb576be276b621e8cd97a3f5b28b0
Cristobal-Sepulveda <sepulveda.cristobal.ignacio@gmail.com>
2020-11-19 00:35:54 -0300

    FAB working

--- app/src/main/java/com/udacity/shoestore/shoeList/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/shoeList/ShoeListFragment.kt
@@ -1,5 +1,5 @@
+package com.udacity.shoestore.shoeList
 
- package com.udacity.shoestore.shoeList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +10,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoelistBinding
 import com.udacity.shoestore.models.Shoe
@@ -23,14 +25,22 @@ import kotlinx.android.synthetic.main.fragment_shoelist.*
                 val binding: FragmentShoelistBinding = DataBindingUtil.inflate(
                     inflater, R.layout.fragment_shoelist, container, false)
                 viewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
-
                 binding.shoeListViewModel = viewModel
                 binding.lifecycleOwner = this
-
-                floatingActionButton.setOnClickListener { v:View ->
+                binding.shoeListFragment = this
+                binding.button.setOnClickListener{ v: View ->
                     v.findNavController().navigate(R.id.action_shoeListFragment_to_shoeDetailFragment)
                 }
-
+                /*floatingActionButton.setOnClickListener{
+                    it.findNavController().navigate(R.id.action_shoeListFragment_to_shoeDetailFragment)
+                }*/
                 return binding.root
             }
+
+    fun navigationFAB(){
+        floatingActionButton.setOnClickListener { v:View ->
+            v.findNavController().navigate(R.id.action_shoeListFragment_to_shoeDetailFragment)
+        }
+    }
 }
+
